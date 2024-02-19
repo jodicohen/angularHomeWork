61bef4b73a60fa503786c6123a35042a76f7a75a
Your Name <you@example.com>
2024-02-19 09:06:12 +0200

    add home work at angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Rights } from './rights/rights.component';
+import { CardComponent } from './card/card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Rights,
+    CardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/rights/rights.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: "./rights..component.html",
+    styleUrls: ["./rights.component.scss"],
+    selector: "app-rights" 
+})
+export class Rights {
+
+}
